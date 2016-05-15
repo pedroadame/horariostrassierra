5771a332f9f26cd5d47fe80fc653a2d7e51a1879
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2016-05-15 18:05:56 +0200

    Controlador de importacion XML

--- app/controllers/xml_import_controller.rb
+++ app/controllers/xml_import_controller.rb
@@ -1,2 +1,7 @@
 class XmlImportController < ApplicationController
+include XmlImportHelper
+
+  def import
+    import_new_database(params[:file])
+  end
 end
--- app/helpers/xml_import_helper.rb
+++ app/helpers/xml_import_helper.rb
@@ -1,2 +1,26 @@
+require 'nokogiri'
 module XmlImportHelper
+
+  def import_new_database(file_data)
+    file = file_data
+    doc = Nokogiri::XML file.read
+    doc = doc.serialize(encoding: 'UTF-8') { |c| c.format.as_xml }
+    xml = Nokogiri::XML doc
+    Dir.chdir Rails.public_path do
+      schema = Nokogiri::XML::Schema(IO.read('database.xsd'))
+      val = schema.validate(xml)
+      valido = schema.valid? xml
+      replace_db xml.to_s if valido && val.empty?
+    end
+  end
+
+  private
+  def replace_db(new_data)
+    `rake db:schema:load`
+    process_data new_data
+  end
+
+  def process_data(data)
+    # TODO Parse XML
+  end
 end
