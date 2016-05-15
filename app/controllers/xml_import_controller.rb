d704bde76ac5bef4851ba71efd7d0ec9213424b0
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2016-05-15 18:13:53 +0200

    Formulario simple para testear subida de ficheros y controlador de XML

--- app/controllers/xml_import_controller.rb
+++ app/controllers/xml_import_controller.rb
@@ -2,6 +2,13 @@ class XmlImportController < ApplicationController
 include XmlImportHelper
 
   def import
-    import_new_database(params[:file])
+    file = params[:file]
+    if file.content_type == 'text/xml'
+      # import_new_database(params[:file])
+
+    else
+      flash[:info] = 'Solo se admiten archivos XML'
+      redirect_to root_url
+    end
   end
 end
