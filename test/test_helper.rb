bd25a06d56984a5a69dfa6d4b3f52179e96b0b55
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2016-05-22 14:01:49 +0200

    Add helper to create an Hour matching day and hour of testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def create_actual_hour
+    Hour.create!(code: '123', day: Time.now.wday, hour: 1, start: Time.now.beginning_of_hour.hh_mm, end: (Time.now + 1.hour).beginning_of_hour.hh_mm)
+  end
 end
